f18db5ec41013e75f67f31c6fbaf89d6a6be841b
Yi Zhou <yizhou7@outlook.com>
2020-06-14 10:13:30 -0400

    using simpler tatics?

--- test.lean
+++ test.lean
@@ -35,3 +35,23 @@ begin
     assumption,
     assumption,
 end
+
+example (p q r : Prop) : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) :=
+begin
+apply iff.intro,
+  intro h,
+    cases h with hp hqr,
+    cases hqr with hq hr,
+      left, constructor,
+        assumption, assumption,
+      right, constructor,
+        assumption, assumption,
+  intro h,
+    cases h with hpq hpr,
+      cases hpq with hp hq,
+        constructor, assumption,
+        left, assumption,
+      cases hpr with hp hr,
+        constructor,assumption,
+        right, assumption,
+end
